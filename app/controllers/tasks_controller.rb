6c120383fec465944d38c30fa4ca548ee6044708
Chyld Medford <chyld.medford@gmail.com>
2017-08-08 03:37:23 +0000

    fin

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,41 @@
 class TasksController < ApplicationController
-    protect_from_forgery except: [:index, :show, :destroy, :create, :update]
+    # protect_from_forgery except: [:index, :show, :destroy, :create, :update]
 
     def index
-        tasks = Task.all
-        render json: tasks
+        @task = Task.new
+        @tasks = Task.all
+        # render json: tasks
     end
     
     def show
-        task = Task.find(params[:id])
-        render json: task
+        # task = Task.find(params[:id])
+        # render json: task
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+        @tasks = Task.all
+        render :index
     end
 
     def destroy
         task = Task.find(params[:id])
         task.destroy
-        head :no_content
+        redirect_to tasks_url
+        # head :no_content
     end
     
     def create
-        task = Task.new params[:task].permit(:title, :priority)
+        task = Task.new params[:task].permit(:title, :priority, :due)
         task.save
-        head :no_content
+        redirect_to tasks_url
+        # head :no_content
     end
     
     def update
         task = Task.find(params[:id])
-        task.update(params[:task].permit(:title, :priority))
-        head :no_content
+        task.update(params[:task].permit(:title, :priority, :due))
+        redirect_to tasks_url
+        # head :no_content
     end
 end
